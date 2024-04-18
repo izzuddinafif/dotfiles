aba3c5cd6e9e1491dd56f22c928a2153a04b248a
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2024-04-18 09:16:26 +0700

    Backup specific config files for starship, fish, kitty, and bat

---
+++ kitty/test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import sys
+
+def draw_tab(screen):
+    screen.draw("Hello Kitty")
+
+if __name__ == "__main__":
+    print("This script cannot be run from the command line.")
